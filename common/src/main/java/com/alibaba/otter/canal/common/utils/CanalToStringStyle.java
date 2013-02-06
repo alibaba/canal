4ee1165e5f6778bbde3c7c45a39a0e2e83f89fd4
agapple <jianghang115@gmail.com>
2013-02-06 10:48:32 +0800

    remove old alibaba Copyright

--- common/src/main/java/com/alibaba/otter/canal/common/utils/CanalToStringStyle.java
+++ common/src/main/java/com/alibaba/otter/canal/common/utils/CanalToStringStyle.java
@@ -1,8 +1,3 @@
-/*
- * Copyright 1999-2004 Alibaba.com All right reserved. This software is the confidential and proprietary information of
- * Alibaba.com ("Confidential Information"). You shall not disclose such Confidential Information and shall use it only
- * in accordance with the terms of the license agreement you entered into with Alibaba.com.
- */
 package com.alibaba.otter.canal.common.utils;
 
 import java.text.SimpleDateFormat;
--- instance/spring/src/test/java/com/alibaba/otter/canal/instance/spring/integrated/DefaultSpringInstanceTest.java
+++ instance/spring/src/test/java/com/alibaba/otter/canal/instance/spring/integrated/DefaultSpringInstanceTest.java
@@ -1,18 +1,3 @@
-/**
- * Project: otter.canal.instance.spring-1.0.0
- * 
- * File Created at 2012-7-13
- * $Id$
- * 
- * Copyright 1999-2100 Alibaba.com Corporation Limited.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * Alibaba Company. ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Alibaba.com.
- */
 package com.alibaba.otter.canal.instance.spring.integrated;
 
 import org.junit.After;
