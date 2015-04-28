c3f2ad08cd734b32ab4d77cb753840fa9e5240a0
agapple <jianghang.loujh@alibaba-inc.com>
2015-04-28 21:33:28 +0800

    fixed warning

--- common/src/main/java/com/google/common/collect/MigrateMap.java
+++ common/src/main/java/com/google/common/collect/MigrateMap.java
@@ -6,11 +6,13 @@ import com.google.common.base.Function;
 
 public class MigrateMap {
 
+    @SuppressWarnings("deprecation")
     public static <K, V> ConcurrentMap<K, V> makeComputingMap(MapMaker maker,
                                                               Function<? super K, ? extends V> computingFunction) {
         return maker.makeComputingMap(computingFunction);
     }
 
+    @SuppressWarnings("deprecation")
     public static <K, V> ConcurrentMap<K, V> makeComputingMap(Function<? super K, ? extends V> computingFunction) {
         return new MapMaker().makeComputingMap(computingFunction);
     }
