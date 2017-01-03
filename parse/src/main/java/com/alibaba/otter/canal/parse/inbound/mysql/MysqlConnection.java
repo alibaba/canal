f45f1ac74f14be29c9ab7531ebb768b32614190b
z070204z <310409139@qq.com>
2017-01-03 18:03:42 +0800

    Update MysqlConnection.java
    修复mysql5.6以下报java.lang.NullPointerException

--- parse/src/main/java/com/alibaba/otter/canal/parse/inbound/mysql/MysqlConnection.java
+++ parse/src/main/java/com/alibaba/otter/canal/parse/inbound/mysql/MysqlConnection.java
@@ -281,7 +281,7 @@ public class MysqlConnection implements ErosaConnection {
         }
 
         List<String> columnValues = rs.getFieldValues();
-        if (columnValues != null && columnValues.size() >= 1 && columnValues.get(0).toUpperCase().equals("CRC32")) {
+        if (columnValues != null && columnValues.size() >= 1 && columnValues.get(0)!=null && columnValues.get(0).toUpperCase().equals("CRC32")) {
             binlogChecksum = LogEvent.BINLOG_CHECKSUM_ALG_CRC32;
         } else {
             binlogChecksum = LogEvent.BINLOG_CHECKSUM_ALG_OFF;
