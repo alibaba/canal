a3b9f6f1ebb21dd528effcceba2ac207f40b15e8
agapple <jianghang.loujh@alibaba-inc.com>
2017-11-14 23:44:15 +0800

    fixed NPE

--- parse/src/main/java/com/alibaba/otter/canal/parse/inbound/mysql/ddl/DruidDdlParser.java
+++ parse/src/main/java/com/alibaba/otter/canal/parse/inbound/mysql/ddl/DruidDdlParser.java
@@ -187,7 +187,7 @@ public class DruidDdlParser {
     }
 
     public static String unescapeName(String name) {
-        if (name.length() > 2) {
+        if (name != null && name.length() > 2) {
             char c0 = name.charAt(0);
             char x0 = name.charAt(name.length() - 1);
             if ((c0 == '"' && x0 == '"') || (c0 == '`' && x0 == '`')) {
@@ -199,7 +199,7 @@ public class DruidDdlParser {
     }
 
     public static String unescapeQuotaName(String name) {
-        if (name.length() > 2) {
+        if (name != null && name.length() > 2) {
             char c0 = name.charAt(0);
             char x0 = name.charAt(name.length() - 1);
             if (c0 == '\'' && x0 == '\'') {
