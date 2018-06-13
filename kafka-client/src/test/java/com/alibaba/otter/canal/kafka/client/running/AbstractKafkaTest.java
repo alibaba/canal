e23a19cd8555b5ccd00e9f7cf317a8541bf5b72a
rewerma <rewerma@163.com>
2018-06-13 18:29:15 +0800

    整理代码

--- kafka-client/src/test/java/com/alibaba/otter/canal/kafka/client/running/AbstractKafkaTest.java
+++ kafka-client/src/test/java/com/alibaba/otter/canal/kafka/client/running/AbstractKafkaTest.java
@@ -13,7 +13,7 @@ public abstract class AbstractKafkaTest {
     public static String topic = "example";
     public static Integer partition = null;
     public static String groupId    = "g1";
-    public static String servers    = "slave1.test.apitops.com:6667,slave2.test.apitops.com:6667,slave3.test.apitops.com:6667";
+    public static String servers    = "slave1:6667,slave2:6667,slave3:6667";
 
     public void sleep(long time) {
         try {
