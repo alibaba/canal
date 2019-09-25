fff623acb112fe28867e3314d5e8ecd41e9fd0e0
EdgarScott <46462768+EdgarScott@users.noreply.github.com>
2019-09-25 11:53:24 +0100

    Add unit tests (#2235)
    * Add Unit Tests for the following classes:
    
    com.alibaba.otter.canal.client.adapter.support.JdbcTypeUtilTest
    com.alibaba.otter.canal.client.adapter.support.UtilTest
    com.taobao.tddl.dbsync.binlog.JsonConversion_Json_ValueTest
    com.taobao.tddl.dbsync.binlog.JsonDiffConversionTest
    com.taobao.tddl.dbsync.binlog.LogBufferTest
    com.taobao.tddl.dbsync.binlog.LogEventTest
    com.taobao.tddl.dbsync.binlog.event.LogHeaderTest
    com.taobao.tddl.dbsync.binlog.event.RowsLogBufferTest
    com.alibaba.otter.canal.parse.driver.mysql.packets.client.RegisterSlaveCommandPacketTest
    com.alibaba.otter.canal.parse.driver.mysql.utils.ByteHelperTest
    com.alibaba.otter.canal.parse.inbound.mysql.ddl.DdlResultTest
    com.alibaba.otter.canal.parse.inbound.mysql.ddl.DruidDdlParserTest
    
    These tests were written using Diffblue Cover
    
    * Add dependencies and refactor

---
+++ dbsync/src/test/java/com/taobao/tddl/dbsync/binlog/JsonConversion_Json_ValueTest.java
@@ -0,0 +1,54 @@
+package com.taobao.tddl.dbsync.binlog;
+
+import com.taobao.tddl.dbsync.binlog.JsonConversion.Json_Value;
+import com.taobao.tddl.dbsync.binlog.JsonConversion.Json_enum_type;
+import com.taobao.tddl.dbsync.binlog.JsonConversion;
+import com.taobao.tddl.dbsync.binlog.LogBuffer;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Timeout;
+
+import java.lang.reflect.Method;
+
+public class JsonConversion_Json_ValueTest {
+
+  @Rule public final ExpectedException thrown = ExpectedException.none();
+
+  @Rule public final Timeout globalTimeout = new Timeout(10000);
+
+  /* testedClasses: JsonConversion_Json_Value */
+  // Test written by Diffblue Cover.
+
+  @Test
+  public void constructorInputNullNotNullOutputVoid() {
+
+    // Arrange
+    final Json_enum_type t = null;
+    final String value = ",";
+
+    // Act, creating object to test constructor
+    final Json_Value objectUnderTest = new Json_Value(t, value);
+
+    // Assert side effects
+    Assert.assertEquals(",", objectUnderTest.m_string_value);
+  }
+
+  // Test written by Diffblue Cover.
+  @Test
+  public void parse_valueInputZeroNotNullPositiveNotNullOutputIllegalArgumentException() {
+
+    // Arrange
+    final int type = 0;
+    final LogBuffer buffer = new LogBuffer();
+    final long len = 4_294_967_297L;
+    final String charsetName = "3";
+
+    // Act
+    thrown.expect(IllegalArgumentException.class);
+    JsonConversion.parse_value(type, buffer, len, charsetName);
+
+    // Method is not expected to return due to exception thrown
+  }
+}
---
+++ driver/src/test/java/com/alibaba/otter/canal/parse/driver/mysql/packets/client/RegisterSlaveCommandPacketTest.java
@@ -0,0 +1,59 @@
+package com.alibaba.otter.canal.parse.driver.mysql.packets.client;
+
+import com.alibaba.otter.canal.parse.driver.mysql.packets.client.RegisterSlaveCommandPacket;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Timeout;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+
+public class RegisterSlaveCommandPacketTest {
+
+  @Rule public final ExpectedException thrown = ExpectedException.none();
+
+  @Rule public final Timeout globalTimeout = new Timeout(10000);
+
+  /* testedClasses: RegisterSlaveCommandPacket */
+  // Test written by Diffblue Cover.
+  @Test
+  public void toBytesOutput27() throws IOException, InvocationTargetException {
+
+    // Arrange
+    final RegisterSlaveCommandPacket objectUnderTest = new RegisterSlaveCommandPacket();
+    objectUnderTest.serverId = 0L;
+    objectUnderTest.reportPort = 0;
+    objectUnderTest.reportPasswd = "foo";
+    objectUnderTest.reportHost = "foo";
+    objectUnderTest.reportUser = "foo";
+    objectUnderTest.setCommand((byte)0);
+
+    // Act
+    final byte[] actual = objectUnderTest.toBytes();
+
+    // Assert result
+    Assert.assertArrayEquals(
+        new byte[] {(byte)0,   (byte)0,   (byte)0,   (byte)0,   (byte)0,   (byte)3,   (byte)102,
+                    (byte)111, (byte)111, (byte)3,   (byte)102, (byte)111, (byte)111, (byte)3,
+                    (byte)102, (byte)111, (byte)111, (byte)0,   (byte)0,   (byte)0,   (byte)0,
+                    (byte)0,   (byte)0,   (byte)0,   (byte)0,   (byte)0,   (byte)0},
+        actual);
+  }
+
+  // Test written by Diffblue Cover.
+  @Test
+  public void toLHInputZeroOutput4() {
+
+    // Arrange
+    final int n = 0;
+
+    // Act
+    final byte[] actual = RegisterSlaveCommandPacket.toLH(n);
+
+    // Assert result
+    Assert.assertArrayEquals(new byte[] {(byte)0, (byte)0, (byte)0, (byte)0}, actual);
+  }
+}
