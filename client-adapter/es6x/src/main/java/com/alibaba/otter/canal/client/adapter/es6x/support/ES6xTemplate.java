1f8f769b902ba6f96b2145565c199ea6071ee3f7
TKaxv-7S <954067342@qq.com>
2020-09-04 15:41:03 +0800

    修复部分sql语法导致的空指针问题，例：SELECT 0 type

--- client-adapter/es6x/src/main/java/com/alibaba/otter/canal/client/adapter/es6x/support/ES6xTemplate.java
+++ client-adapter/es6x/src/main/java/com/alibaba/otter/canal/client/adapter/es6x/support/ES6xTemplate.java
@@ -2,11 +2,7 @@ package com.alibaba.otter.canal.client.adapter.es6x.support;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
