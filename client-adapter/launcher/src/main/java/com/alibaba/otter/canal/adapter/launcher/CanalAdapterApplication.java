17707559c894c5bf1fae69c3cadf1dc3c66c965b
lihuav2017 <614371384@qq.com>
2021-01-07 02:13:10 +0800

    canal adapter phoenix

--- client-adapter/launcher/src/main/java/com/alibaba/otter/canal/adapter/launcher/CanalAdapterApplication.java
+++ client-adapter/launcher/src/main/java/com/alibaba/otter/canal/adapter/launcher/CanalAdapterApplication.java
@@ -13,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CanalAdapterApplication {
     public static void main(String[] args) {
+
         SpringApplication application = new SpringApplication(CanalAdapterApplication.class);
         application.setBannerMode(Banner.Mode.OFF);
         application.run(args);
