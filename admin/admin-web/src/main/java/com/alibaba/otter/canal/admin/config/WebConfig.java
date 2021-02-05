6c01aed067b17e9f39031de6d20ea657597f7eff
jingfeng <jingfeng@pinduoduo.com>
2021-02-05 14:39:19 +0800

    fix information disclosure security vulnerability.

--- admin/admin-web/src/main/java/com/alibaba/otter/canal/admin/config/WebConfig.java
+++ admin/admin-web/src/main/java/com/alibaba/otter/canal/admin/config/WebConfig.java
@@ -84,7 +84,7 @@ public class WebConfig implements WebMvcConfigurer {
             }
         })
             .addPathPatterns("/api/**")
-            .excludePathPatterns("/api/**/config/**")
+            .excludePathPatterns("/api/*/config/**")
             .excludePathPatterns("/api/**/user/login")
             .excludePathPatterns("/api/**/user/logout")
             .excludePathPatterns("/api/**/user/info");
