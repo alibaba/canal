c0c2fc17f0d74c60f88155f21e501cbdffcb3395
Wu Jian Ping <wujp@greatld.com>
2021-07-31 19:39:47 +0800

    解决大数据量下es全量同步无法完成错误

--- client-adapter/es7x/src/main/java/com/alibaba/otter/canal/client/adapter/es7x/etl/ESEtlService.java
+++ client-adapter/es7x/src/main/java/com/alibaba/otter/canal/client/adapter/es7x/etl/ESEtlService.java
@@ -16,6 +16,7 @@ import org.elasticsearch.search.SearchHit;
 
 import com.alibaba.otter.canal.client.adapter.es.core.config.ESSyncConfig;
 import com.alibaba.otter.canal.client.adapter.es.core.config.ESSyncConfig.ESMapping;
+import com.alibaba.otter.canal.client.adapter.es.core.config.ESSyncConfig.JoinSetting;
 import com.alibaba.otter.canal.client.adapter.es.core.config.SchemaItem.FieldItem;
 import com.alibaba.otter.canal.client.adapter.es.core.support.ESBulkRequest;
 import com.alibaba.otter.canal.client.adapter.es.core.support.ESBulkRequest.ESBulkResponse;
@@ -30,6 +31,15 @@ import com.alibaba.otter.canal.client.adapter.support.AdapterConfig;
 import com.alibaba.otter.canal.client.adapter.support.EtlResult;
 import com.alibaba.otter.canal.client.adapter.support.Util;
 
+import com.alibaba.otter.canal.client.adapter.support.DatasourceConfig;
+import java.util.concurrent.ExecutorService;
+import java.util.ArrayList;
+import com.alibaba.druid.pool.DruidDataSource;
+import java.util.concurrent.Future;
+import com.google.common.base.Joiner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * ES ETL Service
  *
@@ -38,9 +48,13 @@ import com.alibaba.otter.canal.client.adapter.support.Util;
  */
 public class ESEtlService extends AbstractEtlService {
 
+    private static final Logger logger = LoggerFactory.getLogger(AbstractEtlService.class);
+
     private ESConnection esConnection;
     private ESTemplate esTemplate;
     private ESSyncConfig config;
+    private final long CNT_PER_TASK = 10000L;
+    private final String type = "ES";
 
     public ESEtlService(ESConnection esConnection, ESSyncConfig config) {
         super("ES", config);
@@ -55,6 +69,121 @@ public class ESEtlService extends AbstractEtlService {
         String sql = mapping.getSql();
         logger.info("sql:{}", sql);
         return importData(sql, params);
+    }
+
+    // Added by Wu Jian Ping, Instread of AbstractEtlService.importData
+    protected EtlResult importData(String sql, List<String> params) {
+        EtlResult etlResult = new EtlResult();
+        AtomicLong impCount = new AtomicLong();
+        List<String> errMsg = new ArrayList<>();
+        if (config == null) {
+            logger.warn("{} mapping config is null, etl go end ", type);
+            etlResult.setErrorMessage(type + "mapping config is null, etl go end ");
+            return etlResult;
+        }
+
+        long start = System.currentTimeMillis();
+        try {
+            DruidDataSource dataSource = DatasourceConfig.DATA_SOURCES.get(config.getDataSourceKey());
+
+            List<Object> values = new ArrayList<>();
+            // 拼接条件
+            if (config.getMapping().getEtlCondition() != null && params != null) {
+                String etlCondition = config.getMapping().getEtlCondition();
+                for (String param : params) {
+                    etlCondition = etlCondition.replace("{}", "?");
+                    values.add(param);
+                }
+
+                sql += " " + etlCondition;
+            }
+
+            if (logger.isDebugEnabled()) {
+                logger.debug("etl sql : {}, values:{}", sql, values);
+            }
+
+            JoinSetting joinSetting = null;
+            for (JoinSetting setting : config.getEsMapping().getJoinSettings().values()) {
+                if (setting.getPrimary()) {
+                    joinSetting = setting;
+                    break;
+                }
+            }
+
+            if (joinSetting == null) {
+                logger.error("Cannot find primry table defination in joinSettings");
+                throw new RuntimeException("Cannot find primry table defination in joinSettings");
+            }
+
+            // 获取总数，这边直接通过主表获取，假如存在条件的话，拼接一下条件
+            String countSql = "SELECT COUNT(1) FROM " + joinSetting.getTableName() + " " + joinSetting.getAliasName();
+            if (config.getMapping().getEtlCondition() != null && params != null) {
+                String etlCondition = config.getMapping().getEtlCondition();
+                for (int i = 0; i < params.size(); ++i) {
+                    etlCondition = etlCondition.replace("{}", "?");
+                }
+
+                countSql += " " + etlCondition;
+            }
+
+            if (logger.isDebugEnabled()) {
+                logger.debug("count sql : {}, values:{}", countSql, values);
+            }
+
+            long cnt = (Long) Util.sqlRS(dataSource, countSql, values, rs -> {
+                Long count = null;
+                try {
+                    if (rs.next()) {
+                        count = ((Number) rs.getObject(1)).longValue();
+                    }
+                } catch (Exception e) {
+                    logger.error(e.getMessage(), e);
+                }
+                return count == null ? 0L : count;
+            });
+
+            // 当大于1万条记录时开启多线程
+            if (cnt >= 10000) {
+                int threadCount = Runtime.getRuntime().availableProcessors();
+
+                long offset;
+                long size = CNT_PER_TASK;
+                long workerCnt = cnt / size + (cnt % size == 0 ? 0 : 1);
+
+                if (logger.isDebugEnabled()) {
+                    logger.debug("workerCnt {} for cnt {} threadCount {}", workerCnt, cnt, threadCount);
+                }
+
+                ExecutorService executor = Util.newFixedThreadPool(threadCount, 5000L);
+                List<Future<Boolean>> futures = new ArrayList<>();
+                for (long i = 0; i < workerCnt; i++) {
+                    offset = size * i;
+                    String sqlFinal = sql + " LIMIT " + offset + "," + size;
+                    Future<Boolean> future = executor.submit(() -> executeSqlImport(dataSource, sqlFinal, values,
+                            config.getMapping(), impCount, errMsg));
+                    futures.add(future);
+                }
+
+                for (Future<Boolean> future : futures) {
+                    future.get();
+                }
+                executor.shutdown();
+            } else {
+                executeSqlImport(dataSource, sql, values, config.getMapping(), impCount, errMsg);
+            }
+
+            logger.info("ES7 数据全量导入完成, 一共导入 {} 条数据, 耗时: {}", impCount.get(), System.currentTimeMillis() - start);
+            etlResult.setResultMessage("导入" + type + " 数据：" + impCount.get() + " 条");
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            errMsg.add(type + " 数据导入异常 =>" + e.getMessage());
+        }
+        if (errMsg.isEmpty()) {
+            etlResult.setSucceeded(true);
+        } else {
+            etlResult.setErrorMessage(Joiner.on("\n").join(errMsg));
+        }
+        return etlResult;
     }
 
     protected boolean executeSqlImport(DataSource ds, String sql, List<Object> values,
