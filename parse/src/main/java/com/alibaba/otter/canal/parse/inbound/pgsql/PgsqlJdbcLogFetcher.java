449780b3df11eb373d77c33675746c7d6f9d3ffe
xiaoma20082008 <chunxiao_ma@outlook.com>
2021-12-08 14:46:57 +0800

    <fix>: create slot if it's not exists

--- parse/src/main/java/com/alibaba/otter/canal/parse/inbound/pgsql/PgsqlJdbcLogFetcher.java
+++ parse/src/main/java/com/alibaba/otter/canal/parse/inbound/pgsql/PgsqlJdbcLogFetcher.java
@@ -2,6 +2,8 @@ package com.alibaba.otter.canal.parse.inbound.pgsql;
 
 import com.alibaba.otter.canal.parse.driver.pgsql.PgsqlJdbcConnector;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public abstract class PgsqlJdbcLogFetcher extends PgsqlLogFetcher {
@@ -11,8 +13,33 @@ public abstract class PgsqlJdbcLogFetcher extends PgsqlLogFetcher {
   @Override
   protected final void doStart(Long pos) throws SQLException {
     this.jdbcConnection = ((PgsqlJdbcConnector) pgsqlConnection.connector).getChannel();
+    checkDestination();
     start0(pos);
   }
 
   protected abstract void start0(Long pos) throws SQLException;
+
+  private void checkDestination() throws SQLException {
+    String countSql = "select count(*) as t from pg_replication_slots where slot_name='" + this.destination + "'";
+    String createSql = "select * from pg_create_logical_replication_slot('" + this.destination + "', 'test_decoding')";
+    boolean exists = false;
+    try (PreparedStatement ps = jdbcConnection.prepareStatement(countSql);
+        ResultSet rs = ps.executeQuery();) {
+      while (rs.next()) {
+        int total = rs.getInt("t");
+        exists = total == 1;
+      }
+    }
+    if (exists) {
+      return;
+    }
+    try (PreparedStatement ps = jdbcConnection.prepareStatement(createSql);
+        ResultSet rs = ps.executeQuery();) {
+      while (rs.next()) {
+        String name = rs.getString("slot_name");
+        String lsn = rs.getString("lsn");
+      }
+    }
+  }
+
 }
