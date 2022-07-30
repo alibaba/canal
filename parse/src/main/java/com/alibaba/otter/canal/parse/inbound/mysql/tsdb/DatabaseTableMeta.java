29fe9d702b9f0b1827495049d9f06d1048f74337
昆梧 <914317348@qq.com>
2022-07-30 16:06:27 +0800

    fixed issue #4245

--- parse/src/main/java/com/alibaba/otter/canal/parse/inbound/mysql/tsdb/DatabaseTableMeta.java
+++ parse/src/main/java/com/alibaba/otter/canal/parse/inbound/mysql/tsdb/DatabaseTableMeta.java
@@ -209,10 +209,11 @@ public class DatabaseTableMeta implements TableMetaTSDB {
             for (String schema : schemas) {
                 // filter views
                 packet = connection.query("show full tables from `" + schema + "` where Table_type = 'BASE TABLE'");
+                int tableNameColumnSize = packet.getFieldDescriptors().size();
                 int tableNameColumnIndex = 0; // default index is 0
                 List<String> tables = new ArrayList<>();
-                for (int line = 0; line < packet.getFieldValues().size() / columnSize; line++) {
-                    String table = packet.getFieldValues().get(line * columnSize + tableNameColumnIndex);
+                for (int line = 0; line < packet.getFieldValues().size() / tableNameColumnSize; line++) {
+                    String table = packet.getFieldValues().get(line * tableNameColumnSize + tableNameColumnIndex);
                     String fullName = schema + "." + table;
                     if (blackFilter == null || !blackFilter.filter(fullName)) {
                         if (filter == null || filter.filter(fullName)) {
