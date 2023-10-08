7e0061fe939cd307ce7d38e4132e355c4d53f53b
jianghang.loujh <jianghang.loujh@alibaba-inc.com>
2023-10-08 19:31:10 +0800

    build.sh add platform

--- docker/build.sh
+++ docker/build.sh
@@ -14,15 +14,16 @@ case "`uname`" in
 esac
 BASE=${bin_abs_path}
 
-
 if [ "$1" == "admin" ] ; then
     rm -rf $BASE/canal.*.tar.gz ;
     cd $BASE/../ && mvn clean package -Dmaven.test.skip -Denv=release && cd $current_path ;
     cp $BASE/../target/canal.admin-*.tar.gz $BASE/
     docker build --no-cache -t canal/canal-admin $BASE/ -f $BASE/Dockerfile_admin
+    # docker build --platform linux/arm64 --no-cache -t canal/canal-admin $BASE/ -f $BASE/Dockerfile_admin
 else 
     rm -rf $BASE/canal.*.tar.gz ; 
     cd $BASE/../ && mvn clean package -Dmaven.test.skip -Denv=release && cd $current_path ;
     cp $BASE/../target/canal.deployer-*.tar.gz $BASE/
     docker build --no-cache -t canal/canal-server $BASE/
+    # docker build -platform linux/arm64 --no-cache -t canal/canal-server $BASE/
 fi
