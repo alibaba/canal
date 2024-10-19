8a469c85f698359fb6530680e77130645fb73d9b
BruceAko <chongzhi@hust.edu.cn>
2024-10-19 18:03:50 +0800

    fix typo
    Signed-off-by: BruceAko <chongzhi@hust.edu.cn>

--- charts/tests/test.sh
+++ charts/tests/test.sh
@@ -2,4 +2,4 @@
 
 helm install canal-admin -f ./admin-values.yaml ../canal-admin --dry-run
 
-helm install canal-admin -f ./server-values.yaml ../canal-server --dry-run
+helm install canal-server -f ./server-values.yaml ../canal-server --dry-run
